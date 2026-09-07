ff30a0fc9b7d5394a412aec61e9eb2e27772b1ea
elahinoore27 <elahinoore27@gmail.com>
2026-09-07 15:26:01 +0530

    File Input Uotput inpython

---
+++ PYTHON/File_IO/fiel-basic.py
@@ -0,0 +1,16 @@
+# a="I'm Noore from pune"
+# file=open("noore.txt","w")
+# file.write(a)
+# file.close()
+
+# file=open("noore.txt","r")
+# content=file.read()
+# # content=file.readlines()
+# print(content)
+# file.close()
+
+a="\n I completed my Graduation"
+file=open("noore.txt","a")
+file.write(a)
+file.close()
+
---
+++ PYTHON/File_IO/file-with.py
@@ -0,0 +1,5 @@
+# a="\n Noore is Good person"
+# with open("noore.txt","a") as file:
+#     file.write(a)
+with open("noore.txt","r") as file:
+    print(file.read())
