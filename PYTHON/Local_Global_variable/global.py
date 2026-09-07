8355fc944a40530a9ab8e9b7082e02fbb5178be9
elahinoore27 <elahinoore27@gmail.com>
2026-09-07 15:40:44 +0530

    local and golabal variable

---
+++ PYTHON/Local_Global_variable/global.py
@@ -0,0 +1,20 @@
+# x=78 #global variable
+# def show_value():
+#     x=12
+#      #Local Variable
+#     print(x)
+# show_value()
+#  #Global Variable
+# show_value()
+# print(x)
+
+# x=78 #global variable
+# def show_value():
+#     global x
+#     x=12
+#      #Local Variable
+#     print(x)
+# show_value()
+#  #Global Variable
+# show_value()
+# print(x)
---
+++ PYTHON/Local_Global_variable/local.py
@@ -0,0 +1,6 @@
+def show_value():
+    x=11 #Local Variable
+    print(x)
+show_value()
+x=12 #Global Variable
+show_value()
